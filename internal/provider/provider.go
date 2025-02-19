a5c6e61ea6267ee07034611d4fd8c8d6650634d0
MJ Hardin <mj.hardin@level.co>
2025-02-19 12:03:20 -0800

    added tests

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,6 +2,8 @@ package provider
 
 import (
 	"context"
+	// "io"
+	// "net/http"
 
 	"github.com/open-feature/go-sdk/openfeature"
 )
@@ -12,16 +14,25 @@ type MDUProviderImpl struct{}
 
 func (MDUProviderImpl) Metadata() openfeature.Metadata { return openfeature.Metadata{} }
 func (MDUProviderImpl) BooleanEvaluation(ctx context.Context, flag string, defaultValue bool, evalCtx openfeature.FlattenedContext) openfeature.BoolResolutionDetail {
-	return openfeature.BoolResolutionDetail{}
+	return openfeature.BoolResolutionDetail{Value: BoolFlagValues[flag].FlagValue}
 }
 func (MDUProviderImpl) StringEvaluation(ctx context.Context, flag string, defaultValue string, evalCtx openfeature.FlattenedContext) openfeature.StringResolutionDetail {
-	return openfeature.StringResolutionDetail{}
+	// url := "http://127.0.0.1:23456/get_string_value"
+	// client := &http.Client{}
+	// resp, err := client.Get(url)
+	// if err != nil {
+	// 	return openfeature.StringResolutionDetail{Value: "goodbye, world"}
+	// }
+	// defer resp.Body.Close()
+	// body, _ := io.ReadAll(resp.Body)
+	// FlagValues["dataplane_generation"] = StringFlagValue{FlagKey: "dataplane_generation", FlagValue: "metal.v1"}
+	return openfeature.StringResolutionDetail{Value: StringFlagValues[flag].FlagValue}
 }
 func (MDUProviderImpl) FloatEvaluation(ctx context.Context, flag string, defaultValue float64, evalCtx openfeature.FlattenedContext) openfeature.FloatResolutionDetail {
-	return openfeature.FloatResolutionDetail{}
+	return openfeature.FloatResolutionDetail{Value: FloatFlagValues[flag].FlagValue}
 }
 func (MDUProviderImpl) IntEvaluation(ctx context.Context, flag string, defaultValue int64, evalCtx openfeature.FlattenedContext) openfeature.IntResolutionDetail {
-	return openfeature.IntResolutionDetail{}
+	return openfeature.IntResolutionDetail{Value: IntFlagValues[flag].FlagValue}
 }
 func (MDUProviderImpl) ObjectEvaluation(ctx context.Context, flag string, defaultValue interface{}, evalCtx openfeature.FlattenedContext) openfeature.InterfaceResolutionDetail {
 	return openfeature.InterfaceResolutionDetail{}
@@ -29,5 +40,6 @@ func (MDUProviderImpl) ObjectEvaluation(ctx context.Context, flag string, defaul
 func (MDUProviderImpl) Hooks() []openfeature.Hook { return []openfeature.Hook{} }
 
 func NewProvider() MDUProviderImpl {
+	PopulateFlagValues()
 	return MDUProviderImpl{}
 }
--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -13,16 +13,47 @@ func TestStringEvaluation(t *testing.T) {
 	err := openfeature.SetProvider(provider.NewProvider())
 	assert.NoError(t, err)
 
-	evalCtx := openfeature.NewEvaluationContext("blah1234", map[string]any{
-		"kind":            "organization-id",
-		"organization-id": "blah1234",
-		"redpanda-id":     "redpanda-blah12342",
-		"key":             "redpanda-blah12343",
-		"cloud-provider":  "aws",
-		"anonymous":       true,
-	})
+	evalCtx := openfeature.NewEvaluationContext("", map[string]any{})
 	client := openfeature.NewClient("stringEvalTests")
 	generation, err := client.StringValue(context.Background(), "dataplane_generation", "k8s.v1", evalCtx)
 	assert.NoError(t, err)
 	assert.Equal(t, "metal.v1", generation)
+}
+
+func TestBoolEvaluation(t *testing.T) {
+	err := openfeature.SetProvider(provider.NewProvider())
+	assert.NoError(t, err)
+
+	evalCtx := openfeature.NewEvaluationContext("", map[string]any{})
+	client := openfeature.NewClient("boolEvalTests")
+
+	generation, err := client.BooleanValue(context.Background(), "grant_soil_access", true, evalCtx)
+	assert.NoError(t, err)
+	assert.Equal(t, false, generation)
+}
+
+func TestFloatEvaluation(t *testing.T) {
+	err := openfeature.SetProvider(provider.NewProvider())
+	assert.NoError(t, err)
+
+	evalCtx := openfeature.NewEvaluationContext("", map[string]any{})
+	client := openfeature.NewClient("floatEvalTests")
+
+	generation, err := client.FloatValue(context.Background(), "special_ability_buff_perc", 0.0, evalCtx)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 0.23456, generation)
+}
+
+func TestIntEvaluation(t *testing.T) {
+	err := openfeature.SetProvider(provider.NewProvider())
+	assert.NoError(t, err)
+
+	evalCtx := openfeature.NewEvaluationContext("", map[string]any{})
+	client := openfeature.NewClient("intEvalTests")
+
+	generation, err := client.IntValue(context.Background(), "num_of_special_abilities", 0, evalCtx)
+
+	assert.NoError(t, err)
+	assert.Equal(t, int64(12), generation)
 }
