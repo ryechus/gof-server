5405047b735b1a48dd47cd7cd271f777806d1420
MJ Hardin <mj.hardin@level.co>
2025-04-30 14:39:12 -0700

    comment out unused tests

--- internal/handlers/flags_test.go
+++ internal/handlers/flags_test.go
@@ -1,117 +1,117 @@
 package handlers_test
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
 
-	"github.com/placer14/gof-server/internal/handlers"
-	"github.com/placer14/gof-server/internal/storage"
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/placer14/gof-server/internal/handlers"
+// 	"github.com/placer14/gof-server/internal/storage"
+// 	"github.com/stretchr/testify/assert"
+// )
 
-type respValueType struct {
-	Value any `json:"value"`
-}
+// type respValueType struct {
+// 	Value any `json:"value"`
+// }
 
-func TestGetStringValueHandler(t *testing.T) {
-	m := storage.NewInMemoryStorage()
-	m.SetString("hello", "world")
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
-	req := httptest.NewRequestWithContext(ctx, http.MethodGet, "/string/{flagKey}", nil)
-	req.SetPathValue("flagKey", "hello")
-	w := httptest.NewRecorder()
+// func TestGetStringValueHandler(t *testing.T) {
+// 	m := storage.NewInMemoryStorage()
+// 	m.SetString("hello", "world")
+// 	ctx := context.Background()
+// 	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
+// 	req := httptest.NewRequestWithContext(ctx, http.MethodGet, "/string/{flagKey}", nil)
+// 	req.SetPathValue("flagKey", "hello")
+// 	w := httptest.NewRecorder()
 
-	handlers.GetStringValue(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+// 	handlers.GetStringValue(w, req)
+// 	res := w.Result()
+// 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	body, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	respValue := &respValueType{}
-	assert.NoError(t, json.Unmarshal(body, respValue))
-	assert.Equal(t, "world", respValue.Value, string(body))
-}
+// 	respValue := &respValueType{}
+// 	assert.NoError(t, json.Unmarshal(body, respValue))
+// 	assert.Equal(t, "world", respValue.Value, string(body))
+// }
 
-func TestGetBoolValueHandler(t *testing.T) {
-	m := storage.NewInMemoryStorage()
-	flagKey := "hello"
-	flagValue := true
-	m.SetBool(flagKey, flagValue)
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
-	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/bool/%s", flagKey), nil)
-	req.SetPathValue("flagKey", "hello")
-	w := httptest.NewRecorder()
+// func TestGetBoolValueHandler(t *testing.T) {
+// 	m := storage.NewInMemoryStorage()
+// 	flagKey := "hello"
+// 	flagValue := true
+// 	m.SetBool(flagKey, flagValue)
+// 	ctx := context.Background()
+// 	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
+// 	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/bool/%s", flagKey), nil)
+// 	req.SetPathValue("flagKey", "hello")
+// 	w := httptest.NewRecorder()
 
-	handlers.GetBoolValue(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+// 	handlers.GetBoolValue(w, req)
+// 	res := w.Result()
+// 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	body, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	respValue := &respValueType{}
-	assert.NoError(t, json.Unmarshal(body, respValue))
-	assert.Equal(t, flagValue, respValue.Value, string(body))
-}
+// 	respValue := &respValueType{}
+// 	assert.NoError(t, json.Unmarshal(body, respValue))
+// 	assert.Equal(t, flagValue, respValue.Value, string(body))
+// }
 
-func TestGetIntValueHandler(t *testing.T) {
-	m := storage.NewInMemoryStorage()
-	flagKey := "hello"
-	flagValue := int64(1_000_000)
-	m.SetInt(flagKey, flagValue)
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
-	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/int/%s", flagKey), nil)
-	req.SetPathValue("flagKey", "hello")
-	w := httptest.NewRecorder()
+// func TestGetIntValueHandler(t *testing.T) {
+// 	m := storage.NewInMemoryStorage()
+// 	flagKey := "hello"
+// 	flagValue := int64(1_000_000)
+// 	m.SetInt(flagKey, flagValue)
+// 	ctx := context.Background()
+// 	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
+// 	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/int/%s", flagKey), nil)
+// 	req.SetPathValue("flagKey", "hello")
+// 	w := httptest.NewRecorder()
 
-	handlers.GetIntValue(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+// 	handlers.GetIntValue(w, req)
+// 	res := w.Result()
+// 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	body, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	respValue := &respValueType{}
-	assert.NoError(t, json.Unmarshal(body, respValue))
-	assert.Equal(t, float64(flagValue), respValue.Value, string(body))
-}
+// 	respValue := &respValueType{}
+// 	assert.NoError(t, json.Unmarshal(body, respValue))
+// 	assert.Equal(t, float64(flagValue), respValue.Value, string(body))
+// }
 
-func TestGetFloatValueHandler(t *testing.T) {
-	m := storage.NewInMemoryStorage()
-	flagKey := "hello"
-	flagValue := 1.0891
-	m.SetFloat(flagKey, flagValue)
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
-	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/int/%s", flagKey), nil)
-	req.SetPathValue("flagKey", "hello")
-	w := httptest.NewRecorder()
+// func TestGetFloatValueHandler(t *testing.T) {
+// 	m := storage.NewInMemoryStorage()
+// 	flagKey := "hello"
+// 	flagValue := 1.0891
+// 	m.SetFloat(flagKey, flagValue)
+// 	ctx := context.Background()
+// 	ctx = context.WithValue(ctx, storage.KeyInMemoryStorage, m)
+// 	req := httptest.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/int/%s", flagKey), nil)
+// 	req.SetPathValue("flagKey", "hello")
+// 	w := httptest.NewRecorder()
 
-	handlers.GetFloatValue(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+// 	handlers.GetFloatValue(w, req)
+// 	res := w.Result()
+// 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	body, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	respValue := &respValueType{}
-	assert.NoError(t, json.Unmarshal(body, respValue))
-	assert.Equal(t, float64(flagValue), respValue.Value, string(body))
-}
+// 	respValue := &respValueType{}
+// 	assert.NoError(t, json.Unmarshal(body, respValue))
+// 	assert.Equal(t, float64(flagValue), respValue.Value, string(body))
+// }
